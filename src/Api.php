6eea97f830bba6f13cbef761e86cf66c3fb84243
Max Carter <nicmaxcarter@gmail.com>
2022-09-23 16:24:16 -0400

    first commit

---
+++ src/Api.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Nicmaxcarter\ApiTool;
+
+class Api
+{
+    public static function getData()
+    {
+        return json_decode(
+            file_get_contents('php://input')
+        );
+    }
+
+    public static function respond200($response, $responseMessage)
+    {
+        $responseMessage = json_encode(
+            ['success' => true, 'response' => $responseMessage]
+        );
+
+        $response->getBody()->write($responseMessage);
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+    }
+
+    public static function respond400($response, $responseMessage)
+    {
+        $responseMessage = json_encode(
+            ['success' => false, 'response' => $responseMessage]
+        );
+
+        $response->getBody()->write($responseMessage);
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(400);
+    }
+
+    public static function respond422($response, $responseMessage)
+    {
+        $responseMessage = json_encode(
+            ['success' => true, 'response' => $responseMessage]
+        );
+
+        $response->getBody()->write($responseMessage);
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(422);
+    }
+
+    public static function error($response, $errorMessage)
+    {
+        return Api::respond400(
+            $response,
+            ['error' => $errorMessage]
+        );
+    }
+
+    public static function success($response, $successMessage)
+    {
+        return Api::respond200(
+            $response,
+            ['message' => $successMessage]
+        );
+    }
+
+    public static function basename()
+    {
+        $time = microtime(true);
+        $str = str_replace('.', '', $time);
+        $hex = bin2hex(random_bytes(8));
+
+        return $str . $hex;
+    }
+}
